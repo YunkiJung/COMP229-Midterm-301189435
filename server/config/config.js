e9e314193022496b37585eb2201f0d5605275860
YunkiJung <100543183+YunkiJung@users.noreply.github.com>
2022-10-24 15:42:22 -0400

    mongodb uri changed

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,2 @@
-export const MongoURI = "mongodb://localhost:27017/media";
+export const MongoURI = "mongodb+srv://yunki:yunki1234@cluster0.c6hy5f6.mongodb.net/book?retryWrites=true&w=majority";
 export const Secret = "someSecret";
